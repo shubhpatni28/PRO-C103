4481b7c2ed028bded907b2f2bafb4b4f4de45ec5
shubhpatni28 <73232775+shubhpatni28@users.noreply.github.com>
2021-09-02 20:04:24 +0530

    Update scatter.py

--- 103/scatter.py
+++ 103/scatter.py
@@ -1,5 +1,5 @@
 import plotly.express as px
 import pandas as pd
 df = pd.read_csv("data.csv")
-fig = px.scatter(df,x="country",y = "cases",color = "date",title = "Per Capita Income")
+fig = px.scatter(df,x="date", y = "cases",color = "country",title = "Per Capita Income")
 fig.show()
